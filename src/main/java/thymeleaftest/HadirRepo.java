379d8237b8b94d9f78f4cd89d9d29bc81a248f72
unknown <cfarreltanmel@student.ciputra.ac.id>
2026-05-04 12:19:37 +0800

    First commit

---
+++ src/main/java/thymeleaftest/HadirRepo.java
@@ -0,0 +1,7 @@
+package thymeleaftest;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface HadirRepo extends CrudRepository<Hadir, Integer> {
+    
+}
---
+++ src/main/java/thymeleaftest/TidakHadirRepo.java
@@ -0,0 +1,7 @@
+package thymeleaftest;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TidakHadirRepo extends CrudRepository<TidakHadir, Integer>{
+    
+}
